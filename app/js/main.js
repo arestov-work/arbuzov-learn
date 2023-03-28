4f9af7a42bae386066386bbe6aa1de85a3579e15
arestov-work <arestov.work@gmail.com>
2023-03-28 13:16:47 +0300

    commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,16 @@
 const upBtn = document.querySelector('.up-btn');
 
+const btnShowMenu = document.querySelector('.btn__show-menu');
+const modalMenu = document.querySelector('.modal-menu');
+const modalMenuClose = document.querySelectorAll('.modal-menu__close');
+const mainBody = document.querySelector('.body');
+const modalCategories = document.querySelector('.modal-categories');
+const btnMiniCatalog = document.querySelector('.btn__mini-catalog');
+
+const selectHeader = document.querySelectorAll('.select__header');
+const selectArrow = document.querySelectorAll('.select-arrow');
+
+
 upBtn.addEventListener('click', function() {
   window.scrollTo({
     top: 0,
@@ -7,3 +18,28 @@ upBtn.addEventListener('click', function() {
     behavior: 'smooth'
   });
 });
+
+btnShowMenu.addEventListener('click', function() {
+  modalMenu.classList.remove('window-hidden');
+  mainBody.style.overflow = 'hidden';
+});
+
+btnMiniCatalog.addEventListener('click', function() {
+  modalCategories.classList.remove('window-hidden');
+  mainBody.style.overflow = 'hidden';
+});
+
+for (let i = 0; i < modalMenuClose.length; i++) {
+  modalMenuClose[i].addEventListener('click', function() {
+    modalMenu.classList.add('window-hidden');
+    modalCategories.classList.add('window-hidden');
+    mainBody.style.overflow = 'scroll';
+  });
+}
+
+for (let i = 0; i < selectHeader.length; i++) {
+  selectHeader[i].addEventListener('click', function() {
+    this.parentElement.classList.toggle('is-active');
+    selectArrow[i].classList.toggle('arrow-rotate');
+  });
+}
